7e4de1e7ff14e3a9671026053dfff55b76d57c5a
Subdelegado1J <153597298+Subdelegado1J@users.noreply.github.com>
2023-12-21 10:27:04 +0100

    Add files via upload

---
+++ patrirray.java
@@ -0,0 +1,28 @@
+public class patrirray {
+
+    public static void main(String[] args) {
+
+        /* Esto son pruebas que quería hacer Patri no me acuerdo para qué xD.
+
+       System.out.println("---------------------------------------------------------------------------");
+
+        String [] pruebis = new String[octeto1binario.length()];
+
+        for(int i = 0; i < octeto1binario.length(); i++){
+            pruebis[i] = String.valueOf(octeto1binario.charAt(i));
+            //System.out.print("Array String: ");
+            System.out.print(pruebis[i]);
+        }
+        System.out.println(" ");
+        System.out.println("-------------------------YA----------------");
+        int [] valores = new int[octeto1binario.length()];
+
+        String pruebisString = "";
+        for (int i = 0; i < octeto1binario.length(); i++){
+            valores[i] = Integer.parseInt(pruebis[i]);
+            //System.out.print("Array Int: ");
+            System.out.print(valores[i]);
+        }
+*/
+    }
+}
